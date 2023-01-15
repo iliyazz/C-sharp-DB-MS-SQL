ed70565689177c652a4380373cb67f30ae493910
iliyazz <iliyaz.softuni@gmail.com>
2023-01-15 17:29:56 +0200

    Create 19.Find First 10 Started Projects.sql

---
+++ 06-Exercise CRUD/19.Find First 10 Started Projects.sql
@@ -0,0 +1,14 @@
+--06.Exercise CRUD
+--19.Find First 10 Started Projects
+--Part I – Queries for SoftUni Database.
+SELECT TOP(10) *
+FROM Projects AS p
+ORDER BY 
+p.StartDate,
+P.[Name]
+
+
+
+
+
+
