595a9ce66293e52cb6f4564c557a3e969d3b904d
iliyazz <iliyaz.softuni@gmail.com>
2023-01-15 11:07:21 +0200

    Create 02.Create Tables.sql

---
+++ 04-Exercise Databases Introduction/02.Create Tables.sql
@@ -0,0 +1,12 @@
+--04.Exercise: Databases Introduction
+--Exercise 2, Create Tables
+CREATE TABLE [Minions] (
+	[Id] INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Age] INT NOT NULL
+)
+
+CREATE TABLE [Towns](
+	[Id] INT PRIMARY KEY,
+	[Name] NVARCHAR (70) NOT NULL
+)
