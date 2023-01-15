5f2130f5ed86a284b0728ce2f1d6af75389f16d8
iliyazz <iliyaz.softuni@gmail.com>
2023-01-15 11:34:26 +0200

    Create 22.Increase Employees Salary.sql

---
+++ 04-Exercise Databases Introduction/22.Increase Employees Salary.sql
@@ -0,0 +1,8 @@
+--04.Exercise: Databases Introduction
+--Exercise 22 Increase Employees Salary
+USE Softuni
+
+UPDATE Employees
+SET Salary = Salary * 1.1 FROM Employees
+
+SELECT Salary FROM Employees
