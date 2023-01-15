f3448a3242f62adfea6007ae1b654fd4fc4ea75c
iliyazz <iliyaz.softuni@gmail.com>
2023-01-15 14:26:25 +0200

    Create 08.Find all Information About Employees.sql

---
+++ 06-Exercise CRUD/08.Find all Information About Employees.sql
@@ -0,0 +1,7 @@
+--06.Exercise CRUD
+
+--08.Find all Information About Employees
+--Part I – Queries for SoftUni Database
+SELECT *
+FROM Employees AS e
+WHERE e.[JobTitle] IN ('Sales Representative')
