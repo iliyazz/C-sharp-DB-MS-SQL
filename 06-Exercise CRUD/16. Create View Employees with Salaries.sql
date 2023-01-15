11fc6008cca845420c27ca690a645ab77d9013e4
iliyazz <iliyaz.softuni@gmail.com>
2023-01-15 17:08:01 +0200

    Create 16. Create View Employees with Salaries.sql

---
+++ 06-Exercise CRUD/16. Create View Employees with Salaries.sql
@@ -0,0 +1,12 @@
+--06.Exercise CRUD
+--16. Create View Employees with Salaries
+--Part I – Queries for SoftUni Database.
+
+CREATE VIEW V_EmployeesSalaries
+AS
+SELECT e.FirstName
+,e.LastName
+,e.Salary
+FROM Employees AS e
+
+--SELECT * FROM V_EmployeesSalaries
