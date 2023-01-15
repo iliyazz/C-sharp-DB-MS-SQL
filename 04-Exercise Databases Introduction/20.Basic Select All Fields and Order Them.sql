98c7a149fb773f5910239bfdafb49effa5071540
iliyazz <iliyaz.softuni@gmail.com>
2023-01-15 11:34:11 +0200

    Create 20.Basic Select All Fields and Order Them.sql

---
+++ 04-Exercise Databases Introduction/20.Basic Select All Fields and Order Them.sql
@@ -0,0 +1,5 @@
+--04.Exercise: Databases Introduction
+--Exercise 20 Basic Select All Fields and Order Them
+SELECT * FROM Towns ORDER BY [Name] ASC
+SELECT * FROM Departments ORDER BY [Name] ASC
+SELECT * FROM Employees ORDER BY Salary DESC
