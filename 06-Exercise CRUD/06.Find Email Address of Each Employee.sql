4e95ffc414538296954ee95f6d59f1e8735f91a5
iliyazz <iliyaz.softuni@gmail.com>
2023-01-15 12:42:18 +0200

    Create 06.Find Email Address of Each Employee.sql

---
+++ 06-Exercise CRUD/06.Find Email Address of Each Employee.sql
@@ -0,0 +1,5 @@
+--06.Exercise CRUD
+--06.Find Email Address of Each Employee
+--Part I – Queries for SoftUni Database
+SELECT e.[FirstName] + '.' + e.[LastName] + '@softuni.bg' AS [Full Email Address]
+FROM Employees AS e
