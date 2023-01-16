7a22c989619e9123f5249d299c2abfc2dd01dda4
iliyazz <iliyaz.softuni@gmail.com>
2023-01-16 18:52:18 +0200

    Update 06.Find Email Address of Each Employee.sql

--- 06-Exercise CRUD/06.Find Email Address of Each Employee.sql
+++ 06-Exercise CRUD/06.Find Email Address of Each Employee.sql
@@ -1,5 +1,6 @@
 --06.Exercise CRUD
 --06.Find Email Address of Each Employee
 --Part I – Queries for SoftUni Database
-SELECT e.[FirstName] + '.' + e.[LastName] + '@softuni.bg' AS [Full Email Address]
+--SELECT e.[FirstName] + '.' + e.[LastName] + '@softuni.bg' AS [Full Email Address]
+SELECT CONCAT(e.[FirstName], '.', e.[LastName], '@', 'softuni.bg' )  AS [Full Email Address]
 FROM Employees AS e
