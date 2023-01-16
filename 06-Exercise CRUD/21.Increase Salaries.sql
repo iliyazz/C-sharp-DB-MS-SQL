42ed3e2479a18181ea55dc821cb345be9bd40f31
iliyazz <iliyaz.softuni@gmail.com>
2023-01-16 21:02:40 +0200

    Update 21.Increase Salaries.sql

--- 06-Exercise CRUD/21.Increase Salaries.sql
+++ 06-Exercise CRUD/21.Increase Salaries.sql
@@ -1,9 +1,26 @@
 --06.Exercise CRUD
 --21.Increase Salaries
 --Part I – Queries for SoftUni Database.
-UPDATE Employees
+-- UPDATE Employees
+   -- SET Salary *= 1.12
+ -- WHERE DepartmentID IN (1, 2, 4, 11)
+-- SELECT e.Salary
+  -- FROM Employees AS e
+  
+  UPDATE Employees
    SET Salary *= 1.12
- WHERE DepartmentID IN (1, 2, 4, 11)
+ WHERE DepartmentID IN 
+			(
+			SELECT [DepartmentID]
+			 FROM [Departments]
+			WHERE [Name] IN 
+					(
+					'Engineering',
+					'Tool Design',
+					'Marketing' ,
+					'Information Services'
+					)
+			)
 SELECT e.Salary
   FROM Employees AS e
 
