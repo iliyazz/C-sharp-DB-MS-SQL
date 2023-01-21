625a323e5b1cc88e70f05c9095fa6d277d1b2b0c
iliyazz <iliyaz.softuni@gmail.com>
2023-01-21 16:25:44 +0200

    Update 12.Countries Holding 'A' 3 or More Times.sql

--- 10-Exercise Built-in Functions/12.Countries Holding 'A' 3 or More Times.sql
+++ 10-Exercise Built-in Functions/12.Countries Holding 'A' 3 or More Times.sql
@@ -1,5 +1,6 @@
 --10-Exercise Built-in Functions
 --12.Countries Holding 'A' 3 or More Times
+--first decision
   SELECT 
          c.CountryName
        , c.IsoCode
@@ -7,6 +8,8 @@
    WHERE LEN(c.CountryName) - LEN(REPLACE(LOWER(c.CountryName), 'a', '')) >= 3
 ORDER BY c.IsoCode
 
-
-
-
+--second decision
+  SELECT c.CountryName, c.IsoCode
+    FROM Countries AS c
+   WHERE LOWER(c.CountryName) LIKE '%a%a%a%'
+ORDER BY c.IsoCode
