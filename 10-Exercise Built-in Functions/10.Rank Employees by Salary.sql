75b75287a6593980797be97ab021b8c0fd597bd5
iliyazz <iliyaz.softuni@gmail.com>
2023-01-21 14:40:50 +0200

    Update 10.Rank Employees by Salary.sql

--- 10-Exercise Built-in Functions/10.Rank Employees by Salary.sql
+++ 10-Exercise Built-in Functions/10.Rank Employees by Salary.sql
@@ -5,7 +5,7 @@
          , e.FirstName
          , e.LastName
          , e.Salary
-         , DENSE_RANK() OVER (PARTITION BY e.Salary ORDER BY e.EmployeeID ASC)
+         , DENSE_RANK() OVER (PARTITION BY e.Salary ORDER BY e.EmployeeID ASC) AS [Rank]
     FROM Employees AS e
    WHERE e.Salary BETWEEN 10000 AND 50000
 ORDER BY e.Salary DESC
