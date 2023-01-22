72e2be3d4bc8fdabce50e58af76811fa3938d1b5
iliyazz <iliyaz.softuni@gmail.com>
2023-01-22 23:57:49 +0200

    Create 03.Sales Employees.sql

---
+++ 12-Exercise Subqueries and Joins/03.Sales Employees.sql
@@ -0,0 +1,11 @@
+--12-Exercise Subqueries and Joins
+--03.Sales Employees
+  SELECT
+         e.EmployeeID
+       , e.FirstName
+       , e.LastName
+       , d.[Name] AS DepartmentName
+    FROM Employees AS e
+    JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+   WHERE d.[Name] = 'Sales'
+ORDER BY e.EmployeeID ASC
