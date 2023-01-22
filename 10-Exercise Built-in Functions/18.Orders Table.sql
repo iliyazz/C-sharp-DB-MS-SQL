a9af41e42ba40b9936b3bb4114314b2da1cabee6
iliyazz <iliyaz.softuni@gmail.com>
2023-01-22 16:38:37 +0200

    Create 18.Orders Table.sql

---
+++ 10-Exercise Built-in Functions/18.Orders Table.sql
@@ -0,0 +1,8 @@
+--10-Exercise Built-in Functions
+--18.Orders Table
+SELECT
+       ProductName
+     , OrderDate
+     , DATEADD(DAY, 3, OrderDate) AS [Pay Due]
+     , DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+  FROM Orders
