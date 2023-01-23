4d9a0b510609c446291e588b3890ac7094a5b4e9
iliyazz <iliyaz.softuni@gmail.com>
2023-01-23 18:00:01 +0200

    Create 04.Employee Departments.sql

---
+++ 12-Exercise Subqueries and Joins/04.Employee Departments.sql
@@ -0,0 +1,11 @@
+--12-Exercise Subqueries and Joins
+--04.Employee Departments
+  SELECT TOP 5
+         e.EmployeeID
+       , e.FirstName
+       , e.Salary
+       , d.[Name] AS DepartmentName
+    FROM Employees AS e
+    JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+   WHERE e.Salary > 15000
+ORDER BY d.DepartmentID ASC
