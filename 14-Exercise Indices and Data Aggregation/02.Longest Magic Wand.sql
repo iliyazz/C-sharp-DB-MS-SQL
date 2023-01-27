32ba95711b8324916a1d3d4b382cc61995c309b9
iliyazz <iliyaz.softuni@gmail.com>
2023-01-27 22:52:21 +0200

    Create 02.Longest Magic Wand.sql

---
+++ 14-Exercise Indices and Data Aggregation/02.Longest Magic Wand.sql
@@ -0,0 +1,15 @@
+--13-Indices and Data Aggregation
+--02.Longest Magic Wand
+SELECT
+       MAX([w].[MagicWandSize])
+    AS [LongestMagicWand]
+  FROM [WizzardDeposits]
+    AS [w]
+----------------------------
+SELECT TOP (1)
+           [w].[MagicWandSize]
+        AS [LongestMagicWand]
+      FROM [WizzardDeposits]
+        AS [w]
+  GROUP BY [w].[MagicWandSize]
+  ORDER BY [w].[MagicWandSize] DESC
