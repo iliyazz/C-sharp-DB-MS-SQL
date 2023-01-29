90649a59b4788581612168c55535c3a97337e69f
iliyazz <iliyaz.softuni@gmail.com>
2023-01-29 20:04:27 +0200

    Create 02. Insert.sql

---
+++ 00-exam preparation/03-Databases MSSQL Server Retake Exam - 10 Dec 2021/02. Insert.sql
@@ -0,0 +1,7 @@
+--02. Insert 
+INSERT INTO Passengers(FullName, Email)
+SELECT CONCAT(p.FirstName, ' ', p.LastName) AS FullName,
+CONCAT(p.FirstName, p.LastName, '@gmail.com') AS Email
+FROM Pilots as p
+WHERE p.Id BETWEEN 5 AND 15
+ORDER BY p.Id
