3ebd42efce84cf0db0a3e6041e68b2c502fde965
iliyazz <iliyaz.softuni@gmail.com>
2023-01-29 09:52:18 +0200

    Create 7. Owners and Their Animals.sql

---
+++ 00-exam preparation/02-Databases MSSQL Server Exam - 19 June 2022/7. Owners and Their Animals.sql
@@ -0,0 +1,9 @@
+--07. Owners and Their Animals
+  SELECT TOP (5)
+         o.[Name] AS [Owner]
+       , COUNT(*) AS CountOfAnimals
+    FROM Owners AS o
+    JOIN Animals AS a ON  o.Id = a.OwnerId
+GROUP BY o.[Name]
+ORDER BY CountOfAnimals DESC
+       , o.[Name] ASC
