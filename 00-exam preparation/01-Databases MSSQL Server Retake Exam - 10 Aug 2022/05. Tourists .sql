5ec42d885ddf7d7deeddb3c670d99369e7b7c606
iliyazz <iliyaz.softuni@gmail.com>
2023-01-30 22:08:08 +0200

    Create 05. Tourists .sql

---
+++ 00-exam preparation/01-Databases MSSQL Server Retake Exam - 10 Aug 2022/05. Tourists .sql
@@ -0,0 +1,11 @@
+--05. Tourists 
+  SELECT t.[Name]
+       , t.Age
+       , t.PhoneNumber
+       , t.Nationality
+    FROM Tourists AS t
+ORDER BY t.Nationality ASC, t.Age DESC, t.[Name] ASC
+
+
+
+
