16981fbd461438390904b0bc6206df7ed9e2ba5c
iliyazz <iliyaz.softuni@gmail.com>
2023-01-30 18:44:56 +0200

    Update 07.Deposits Filter.sql

--- 14-Exercise Indices and Data Aggregation/07.Deposits Filter.sql
+++ 14-Exercise Indices and Data Aggregation/07.Deposits Filter.sql
@@ -1,5 +1,6 @@
 --14-Exercise Indices and Data Aggregation
 --07.Deposits Filter
+--first decision
   SELECT
          [w].[DepositGroup]
        , SUM([w].[DepositAmount]) AS [TotalSum]
@@ -7,3 +8,13 @@
 GROUP BY [w].[DepositGroup], [w].[MagicWandCreator]
   HAVING [w].[MagicWandCreator] = 'Ollivander family' AND SUM([w].[DepositAmount]) < 150000
 ORDER BY SUM([w].[DepositAmount]) DESC
+
+--second decision
+  SELECT
+         [w].[DepositGroup]
+       , SUM([w].[DepositAmount]) AS [TotalSum]
+    FROM [WizzardDeposits] AS [w]
+   WHERE [MagicWandCreator] = 'Ollivander family'
+GROUP BY [w].[DepositGroup]
+  HAVING SUM([w].[DepositAmount]) < 150000
+ORDER BY [TotalSum] DESC
