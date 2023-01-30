5e35973d26f84df7e5e03ed37987a428601f220d
iliyazz <iliyaz.softuni@gmail.com>
2023-01-30 21:13:12 +0200

    Update 18.3rd Highest Salary.sql

--- 14-Exercise Indices and Data Aggregation/18.3rd Highest Salary.sql
+++ 14-Exercise Indices and Data Aggregation/18.3rd Highest Salary.sql
@@ -1,5 +1,6 @@
 --14-Exercise Indices and Data Aggregation
 --18.*3rd Highest Salary
+--first decision
   SELECT [ranking].[DepartmentID]
        , [ranking].[Salary] AS [ThirdHighestSalary]
    FROM(
@@ -9,3 +10,14 @@
            FROM [Employees] AS [e]) AS [ranking]
           WHERE [dsrank] = 3
 GROUP BY [ranking].DepartmentID, [ranking].[Salary]	  		  
+
+--second decision
+SELECT DISTINCT DepartmentID
+     , Salary
+ FROM (
+       SELECT e.DepartmentID
+            , e.Salary
+            ,  DENSE_RANK() OVER (PARTITION BY e.DepartmentID ORDER BY e.Salary DESC) AS SalaryRank
+         FROM Employees AS e
+         ) AS SalaryRankingSubquery
+WHERE SalaryRank = 3
