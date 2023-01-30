ced59a3658db6520d16500ee53c16cbdf4a316cf
iliyazz <iliyaz.softuni@gmail.com>
2023-01-30 22:29:37 +0200

    Create 07. Count of Sites in Sofia Province.sql

---
+++ 00-exam preparation/01-Databases MSSQL Server Retake Exam - 10 Aug 2022/07. Count of Sites in Sofia Province.sql
@@ -0,0 +1,12 @@
+--07. Count of Sites in Sofia Province
+ SELECT l.Province
+       , l.Municipality
+       , l.[Name] AS [Location]
+       , COUNT(*) AS CountOfSites
+    FROM Sites AS s
+    JOIN Locations AS l ON s.LocationId= l.Id
+   WHERE l.Province = 'Sofia'
+GROUP BY l.Province, l.Municipality, l.[Name]
+ORDER BY CountOfSites DESC, l.[Name] ASC
+
+
