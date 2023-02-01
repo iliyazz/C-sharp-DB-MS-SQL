0d39eb075e60041e23416175ba7a3b47677cdbf3
iliyazz <iliyaz.softuni@gmail.com>
2023-02-01 19:35:34 +0200

    Create 02. Employees with Salary Above Number.sql

---
+++ 16-Functions and Stored Procedures/02. Employees with Salary Above Number.sql
@@ -0,0 +1,17 @@
+--16-Exercise Functions and Procedures
+--02. Employees with Salary Above Number
+CREATE PROC usp_GetEmployeesSalaryAboveNumber (@SalaryAboveNumber DECIMAL(18, 4))
+AS
+SELECT e.FirstName
+     , e.LastName
+  FROM Employees AS e
+ WHERE e.Salary > @SalaryAboveNumber
+GO
+
+EXEC usp_GetEmployeesSalaryAboveNumber 48100 
+GO
+
+
+
+
+
