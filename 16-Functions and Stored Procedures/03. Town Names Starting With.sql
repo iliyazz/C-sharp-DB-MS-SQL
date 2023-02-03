6950b2576302749589fd1813aa798ce43c7d4a8e
iliyazz <iliyaz.softuni@gmail.com>
2023-02-03 20:45:39 +0200

    Update 03. Town Names Starting With.sql

--- 16-Functions and Stored Procedures/03. Town Names Starting With.sql
+++ 16-Functions and Stored Procedures/03. Town Names Starting With.sql
@@ -6,10 +6,13 @@ SELECT t.[Name] AS Town
   FROM Towns AS t
  WHERE t.[Name] LIKE @StartingWith + '%'
 GO
+--second decision
+CREATE PROC usp_GetTownsStartingWith(@StartingWith NVARCHAR(50))
+AS
+SELECT t.[Name] AS Town
+  FROM Towns AS t
+   WHERE LEFT(t.[Name], LEN(@StartingWith)) = @StartingWith
+GO
 
 EXEC usp_GetTownsStartingWith 'B'
 GO
-
-
-
-
