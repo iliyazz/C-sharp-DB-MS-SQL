6f348d637c1cc6e706291b26f885d9560a302aba
iliyazz <iliyaz.softuni@gmail.com>
2023-02-04 19:27:02 +0200

    Update 10. Cigars by Size.sql

--- 00-exam preparation/04-Databases MSSQL Server Exam - 16 October 2021/10. Cigars by Size.sql
+++ 00-exam preparation/04-Databases MSSQL Server Exam - 16 October 2021/10. Cigars by Size.sql
@@ -1,4 +1,5 @@
 --10. Cigars by Size
+--expected from Judge
   SELECT c.LastName
        , AVG(s.[Length]) AS CigarLength
        , CEILING(AVG(s.RingRange)) AS CigarRingRange
@@ -7,5 +8,15 @@
     JOIN Cigars AS cig ON cc.CigarId = cig.Id
     JOIN Sizes AS s ON cig.SizeId = s.Id
 GROUP BY c.LastName
+ORDER BY CigarLength DESC
+--correct query
+  SELECT c.LastName
+       , AVG(s.[Length]) AS CigarLength
+       , CEILING(AVG(s.RingRange)) AS CigarRingRange
+    FROM Clients AS c
+    JOIN ClientsCigars AS cc ON cc.ClientId = c.Id
+    JOIN Cigars AS cig ON cc.CigarId = cig.Id
+    JOIN Sizes AS s ON cig.SizeId = s.Id
+GROUP BY c.Id, c.LastName
 ORDER BY CigarLength DESC
 
