a49584e14d10efb64e97bee9fa667a88f3658162
iliyazz <iliyaz.softuni@gmail.com>
2023-02-05 14:31:58 +0200

    Create 05. Mechanic Assignments.sql

---
+++ 00-exam preparation/06-Databases MSSQL Server Exam - 27 Jun 2020/05. Mechanic Assignments.sql
@@ -0,0 +1,7 @@
+--05. Mechanic Assignments
+  SELECT CONCAT(m.FirstName, ' ', m.LastName) AS Mechanic
+       , j.[Status]
+       , j.IssueDate
+    FROM Mechanics AS m
+    JOIN Jobs AS j ON m.MechanicId = j.MechanicId
+ORDER BY m.MechanicId ASC, j.IssueDate ASC, j.JobId ASC
