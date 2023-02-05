c9ec38c16afe869e0fe87944958738368e17e72f
iliyazz <iliyaz.softuni@gmail.com>
2023-02-05 14:53:15 +0200

    Create 07. Mechanic Performance.sql

---
+++ 00-exam preparation/06-Databases MSSQL Server Exam - 27 Jun 2020/07. Mechanic Performance.sql
@@ -0,0 +1,7 @@
+--07. Mechanic Performance
+  SELECT CONCAT(m.FirstName, ' ', m.LastName) AS Mechanic
+       , AVG(DATEDIFF(DAY, j.IssueDate, j.FinishDate)) AS [Average Days]
+    FROM Mechanics AS m
+    JOIN Jobs AS j ON m.MechanicId = j.MechanicId
+GROUP BY CONCAT(m.FirstName, ' ', m.LastName), m.MechanicId
+ORDER BY m.MechanicId ASC
