d719a2b2ce482c82cd8c0f24dea32549a553114a
iliyazz <iliyaz.softuni@gmail.com>
2023-02-07 00:38:47 +0200

    Create 07. Employees with Three Projects.sql

---
+++ 17-Triggers and Transactions/07. Employees with Three Projects.sql
@@ -0,0 +1,27 @@
+--17-Triggers and Transactions
+--07. Employees with Three Projects
+CREATE PROCEDURE usp_AssignProject(@emloyeeId INT, @projectID INT)
+AS
+BEGIN
+	DECLARE @NumberOfProjects INT = (
+									SELECT COUNT(ep.EmployeeID)
+									FROM EmployeesProjects AS ep
+									JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+									WHERE ep.EmployeeID = @emloyeeId
+									)
+	BEGIN TRANSACTION
+		INSERT INTO EmployeesProjects
+		VALUES
+		(@emloyeeId, @projectID)
+		IF(@NumberOfProjects >= 3)
+			BEGIN
+				ROLLBACK
+				RAISERROR('The employee has too many projects!', 16, 1)
+				RETURN
+			END
+		ELSE
+			BEGIN
+				COMMIT
+			RETURN
+	END
+END
