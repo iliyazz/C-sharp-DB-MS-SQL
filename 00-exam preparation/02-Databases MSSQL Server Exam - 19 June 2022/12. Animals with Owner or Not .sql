1edbd8a96660076c3f3bd793c76c11c81c70bf2a
iliyazz <iliyaz.softuni@gmail.com>
2023-02-06 20:54:54 +0200

    Update 12. Animals with Owner or Not .sql

--- 00-exam preparation/02-Databases MSSQL Server Exam - 19 June 2022/12. Animals with Owner or Not .sql
+++ 00-exam preparation/02-Databases MSSQL Server Exam - 19 June 2022/12. Animals with Owner or Not .sql
@@ -1,24 +1,12 @@
 --12. Animals with Owner or Not 
-CREATE PROC usp_AnimalsWithOwnersOrNot(@AnimalName VARCHAR(30))
+CREATE /*OR ALTER*/ PROC usp_AnimalsWithOwnersOrNot(@AnimalName VARCHAR(30))
 AS
 BEGIN
-IF (SELECT OwnerId 
-	  FROM Animals
-	 WHERE [Name] = @AnimalName) IS NULL
-	 BEGIN
-		SELECT a.[Name]
-		     , 'For adoption' AS OwnersName
-		  FROM Animals AS a
-		 WHERE a.[Name] = @AnimalName
-	 END
-ELSE
-	BEGIN
-		SELECT a.[Name]
-		     , o.[Name] AS OwnersName
-		  FROM Animals AS a
-		  JOIN Owners AS o ON a.OwnerId = o.Id
-		 WHERE a.[Name] = @AnimalName
-	END
+	SELECT a.[Name]
+	     , ISNULL(o.[Name], 'For adoption') AS OwnersName
+	  FROM Animals AS a
+	  LEFT JOIN Owners AS o ON a.OwnerId = o.Id
+	 WHERE a.[Name] = @AnimalName
 END
 
 --EXEC usp_AnimalsWithOwnersOrNot 'Pumpkinseed Sunfish'
