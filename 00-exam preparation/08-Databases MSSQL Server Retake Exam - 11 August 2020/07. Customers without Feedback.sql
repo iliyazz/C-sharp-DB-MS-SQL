db127b4cf4e3ce1abf58b833300c9cf9fca59f32
iliyazz <iliyaz.softuni@gmail.com>
2023-02-08 20:30:36 +0200

    Create 07. Customers without Feedback.sql

---
+++ 00-exam preparation/08-Databases MSSQL Server Retake Exam - 11 August 2020/07. Customers without Feedback.sql
@@ -0,0 +1,8 @@
+--07. Customers without Feedback
+   SELECT CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
+          c.PhoneNumber, 
+          c.Gender
+     FROM Customers AS c
+LEFT JOIN Feedbacks AS f ON f.CustomerId = c.Id
+    WHERE f.Id IS NULL
+ ORDER BY c.Id ASC
