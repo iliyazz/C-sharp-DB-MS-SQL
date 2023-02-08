f7c6bfb8f1a07ea123f84b81238775de24f569d2
iliyazz <iliyaz.softuni@gmail.com>
2023-02-08 20:03:56 +0200

    Create 03. Update.sql

---
+++ 00-exam preparation/08-Databases MSSQL Server Retake Exam - 11 August 2020/03. Update.sql
@@ -0,0 +1,8 @@
+--03. Update
+UPDATE Ingredients
+SET DistributorId = 35
+WHERE [Name] IN ('Bay Leaf', 'Paprika',  'Poppy')
+
+UPDATE Ingredients
+SET OriginCountryId  = 14
+WHERE OriginCountryId = 8
