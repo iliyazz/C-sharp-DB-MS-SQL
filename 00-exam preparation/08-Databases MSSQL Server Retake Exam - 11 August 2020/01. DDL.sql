57113cc99fc990ccad92f50d3b5bf2ef89da110c
iliyazz <iliyaz.softuni@gmail.com>
2023-02-08 19:53:49 +0200

    Create 01. DDL

---
+++ 00-exam preparation/08-Databases MSSQL Server Retake Exam - 11 August 2020/01. DDL.sql
@@ -0,0 +1,53 @@
+--Databases MSSQL Server Retake Exam - 11 August 2020
+-- CREATE DATABASE Bakery
+-- GO
+-- USE Bakery
+-- GO
+
+--01. DDL
+CREATE TABLE Countries(
+Id INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(50) UNIQUE
+)
+CREATE TABLE Customers(
+Id INT PRIMARY KEY IDENTITY,
+FirstName NVARCHAR(25),
+LastName NVARCHAR(25),
+Gender CHAR CHECK(Gender IN ('M', 'F')),
+Age INT,
+PhoneNumber VARCHAR(10) CHECK(LEN(PhoneNumber) = 10),
+CountryId INT FOREIGN KEY REFERENCES Countries(Id)
+)
+CREATE TABLE Products(
+Id INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(25) UNIQUE,
+[Description] NVARCHAR(250),
+Recipe NVARCHAR(MAX),
+Price MONEY CHECK(Price >= 0)
+)
+CREATE TABLE Feedbacks(
+Id INT PRIMARY KEY IDENTITY,
+[Description] NVARCHAR(255),
+Rate DECIMAL(4,2) CHECK(Rate BETWEEN 0 AND 10),
+ProductId INT FOREIGN KEY REFERENCES Products(Id),
+CustomerId INT FOREIGN KEY REFERENCES Customers(Id),
+)
+CREATE TABLE Distributors(
+Id INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(25) UNIQUE,
+AddressText NVARCHAR(30),
+Summary NVARCHAR(200),
+CountryId INT FOREIGN KEY REFERENCES Countries(Id)
+)
+CREATE TABLE Ingredients(
+Id INT PRIMARY KEY IDENTITY,
+[Name] NVARCHAR(30),
+[Description] NVARCHAR(200),
+OriginCountryId INT FOREIGN KEY REFERENCES Countries(Id),
+DistributorId INT FOREIGN KEY REFERENCES Distributors(Id)
+)
+CREATE TABLE ProductsIngredients(
+ProductId INT FOREIGN KEY REFERENCES Products(Id),
+IngredientId INT FOREIGN KEY REFERENCES Ingredients(Id),
+PRIMARY KEY(ProductId, IngredientId)
+)
