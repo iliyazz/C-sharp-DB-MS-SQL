624463ebb6909aca1c947e691cd5a5657380f6de
iliyazz <iliyaz.softuni@gmail.com>
2023-02-16 20:13:06 +0200

    Update 10. Tourists visiting History & Archaeology sites.sql

--- 00-exam preparation/01-Databases MSSQL Server Retake Exam - 10 Aug 2022/10. Tourists visiting History & Archaeology sites.sql
+++ 00-exam preparation/01-Databases MSSQL Server Retake Exam - 10 Aug 2022/10. Tourists visiting History & Archaeology sites.sql
@@ -1,13 +1,14 @@
 --10. Tourists visiting History & Archaeology sites
-   SELECT DISTINCT SUBSTRING(t.[Name], CHARINDEX(' ', t.[Name]), 50) AS LastName
-        , t.Nationality
-        , t.Age
-        , t.PhoneNumber
-    FROM Tourists AS t
-    JOIN SitesTourists AS st ON t.Id = st.TouristId
-    JOIN Sites AS s ON st.SiteId = s.Id
-    JOIN Categories AS c ON s.CategoryId = c.Id
+  SELECT SUBSTRING(t.[Name], CHARINDEX(' ', t.[Name]), LEN(t.[Name]) + 1 - CHARINDEX(' ', t.[Name])) AS LastName,
+         t.Nationality,
+         t.Age,
+         t.PhoneNumber
+    FROM Categories AS c
+    JOIN Sites AS s ON c.Id = s.CategoryId
+    JOIN SitesTourists AS st ON s.Id = st.SiteId
+    JOIN Tourists AS t ON st.TouristId = t.Id
    WHERE c.[Name] = 'History and archaeology'
+GROUP BY t.[Name], t.Nationality, t.Age, t.PhoneNumber
 ORDER BY LastName ASC
 
 
